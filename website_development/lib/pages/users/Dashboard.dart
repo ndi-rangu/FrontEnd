0aa4b60284aa5bbebf5c1d864b6aa32309b69d86
ndi-rangu <bnjaramba2010@gmail.com>
2024-02-17 13:25:39 +0300

    Update Dashboard.dart

--- website_development/lib/pages/users/Dashboard.dart
+++ website_development/lib/pages/users/Dashboard.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, sized_box_for_whitespace, prefer_typing_uninitialized_variables, unused_import
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, sized_box_for_whitespace, prefer_typing_uninitialized_variables, unused_import, prefer_adjacent_string_concatenation
 
 //import 'dart:ffi';
 
@@ -48,7 +48,7 @@ class _DashboardPageState extends State<DashboardPage> {
       body: Container(
         height: MediaQuery.of(context).size.width,
         width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(color: Colors.white),
+        decoration: BoxDecoration(color: Colors.grey[300]),
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
@@ -200,12 +200,17 @@ class _DashboardPageState extends State<DashboardPage> {
                                   ),
                                 ),
                                 SizedBox(width: 50),
-                                Container(
-                                  width: 150,
-                                  height: 70,
-                                  decoration:
-                                      BoxDecoration(color: Colors.black),
-                                ),
+                                ElevatedButton(
+                                  onPressed: (){
+                                    Navigator.pushNamed(context, '/Login');
+                                  },
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all<Color>(Colors.lightBlue[50]?? Colors.lightBlue.shade50),
+                                  ), child: Icon(
+                                    Icons.logout_rounded,
+                                    size: 50,
+                                    color: Colors.black,
+                                  )),
                               ],
                             ),
                           ),
@@ -215,6 +220,330 @@ class _DashboardPageState extends State<DashboardPage> {
                   ],
                 ),
               ),
+
+              SizedBox(height: 20),
+
+              //display users profile details
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Container(
+                      height: 800,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 100,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey,
+                                  offset: Offset(0, 0),
+                                  blurRadius: 10
+                                )],
+                              borderRadius: BorderRadius.vertical(
+                                bottom: Radius.elliptical(40, 40), 
+                                top: Radius.elliptical(10, 10),
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                                  child: Text(
+                                    'PROFILE',
+                                    style: GoogleFonts.autourOne(
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black
+                                    ),
+                                  ),
+                                ),
+                                
+                                Icon(
+                                  Icons.person_rounded,
+                                  size: 50,
+                                  color: Colors.black,
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 10),                         
+
+                          //first and middle name 
+                          Column(
+                            children: [
+                              Container(
+                                height: 125,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey,
+                                offset: Offset(0, 1.5),
+                                blurRadius: 4,
+                                spreadRadius: 0
+                              ),
+                              BoxShadow(
+                                color: Colors.grey,
+                                offset: Offset(0, -1.5),
+                                blurRadius: 4,
+                                spreadRadius: 0
+                              )],
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(vertical:8.0),
+                                      child: Container(
+                                        height: 100,
+                                        width: 600,
+                                        decoration: BoxDecoration(
+                                          color: Colors.white
+                                        ),
+                                        child: Center(
+                                          child: Text(
+                                            'First Name: ' + '${jwtDecodedToken['firstName'] ?? ''}',
+                                            style: GoogleFonts.notoSansJavanese(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.black87,
+                                            ),),
+                                        ),
+                                      ),                                      
+                                    ),
+                                    SizedBox( width: 50),
+                              
+                                    Container(
+                                        height: 100,
+                                        width: 600,
+                                        decoration: BoxDecoration(
+                                          color: Colors.white
+                                        ),
+                                        child: Center(
+                                          child: Text(
+                                              'Middle Name: ${jwtDecodedToken['middleName'] ?? ''}',
+                                              style: GoogleFonts.notoSansJavanese(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold,
+                                              color: Colors.black87
+                                              ),),
+                                        ),                                          
+                                      ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+
+                          //surname(last name) and age
+                          Column(
+                            children: [
+                              Container(
+                                height: 125,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey,
+                                offset: Offset(0, 1.5),
+                                blurRadius: 4,
+                                spreadRadius: 0
+                              ),
+                              BoxShadow(
+                                color: Colors.grey,
+                                offset: Offset(0, -1.5),
+                                blurRadius: 4,
+                                spreadRadius: 0
+                              )],
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(vertical:8.0),
+                                      child: Container(
+                                        height: 100,
+                                        width: 600,
+                                        decoration: BoxDecoration(
+                                          color: Colors.white
+                                        ),
+                                        child: Center(
+                                          child: Text(
+                                            'Surname: ${jwtDecodedToken['surname'] ?? ''}',
+                                            style: GoogleFonts.notoSansJavanese(
+                                              fontSize: 20,
+                                              fontWeight:FontWeight.bold,
+                                              color: Colors.black87
+                                            ),),
+                                        ),
+                                      ),                                      
+                                    ),
+                                    SizedBox( width: 50),
+                              
+                                    Container(
+                                        height: 100,
+                                        width: 600,
+                                        decoration: BoxDecoration(
+                                          color: Colors.white
+                                        ),
+                                        child: Center(
+                                          child: Text(
+                                              'Age: ${jwtDecodedToken['age'] ?? ''}',
+                                              style: GoogleFonts.notoSansJavanese(
+                                                fontSize: 20,
+                                                fontWeight:FontWeight.bold,
+                                              color: Colors.black87
+                                              ),),
+                                        ),                                          
+                                      ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                          
+                          //Gender and email
+                          Column(
+                            children: [
+                              Container(
+                                height: 125,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey,
+                                offset: Offset(0, 1.5),
+                                blurRadius: 4,
+                                spreadRadius: 0
+                              ),
+                              BoxShadow(
+                                color: Colors.grey,
+                                offset: Offset(0, -1.5),
+                                blurRadius: 4,
+                                spreadRadius: 0
+                              )],
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(vertical:8.0),
+                                      child: Container(
+                                        height: 100,
+                                        width: 600,
+                                        decoration: BoxDecoration(
+                                          color: Colors.white
+                                        ),
+                                        child: Center(
+                                          child: Text(
+                                            'Gender: ${jwtDecodedToken['gender'] ?? ''}',
+                                            style: GoogleFonts.notoSansJavanese(
+                                              fontSize: 20,
+                                              fontWeight:FontWeight.bold,
+                                              color: Colors.black87
+                                            ),),
+                                        ),
+                                      ),                                      
+                                    ),
+                                    SizedBox( width: 50),
+                              
+                                    Container(
+                                        height: 100,
+                                        width: 600,
+                                        decoration: BoxDecoration(
+                                          color: Colors.white
+                                        ),
+                                        child: Center(
+                                          child: Text(
+                                              'Email: ${jwtDecodedToken['email'] ?? ''}',
+                                              style: GoogleFonts.notoSansJavanese(
+                                                fontSize: 20,
+                                                fontWeight:FontWeight.bold,
+                                              color: Colors.black87
+                                              ),),
+                                        ),                                          
+                                      ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                          
+                          //position
+                          Column(
+                            children: [
+                              Container(
+                                height: 125,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey,
+                                offset: Offset(0, 1.5),
+                                blurRadius: 4,
+                                spreadRadius: 0
+                              ),
+                              BoxShadow(
+                                color: Colors.grey,
+                                offset: Offset(0, -1.5),
+                                blurRadius: 4,
+                                spreadRadius: 0
+                              )],
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(vertical:8.0),
+                                      child: Container(
+                                        height: 100,
+                                        width: 600,
+                                        decoration: BoxDecoration(
+                                          color: Colors.white
+                                        ),
+                                        child: Center(
+                                          child: Text(
+                                            'Position: ${jwtDecodedToken['position'] ?? ''}',
+                                            style: GoogleFonts.notoSansJavanese(
+                                              fontSize: 20,
+                                              fontWeight:FontWeight.bold,
+                                              color: Colors.black87
+                                            ),),
+                                        ),
+                                      ),                                      
+                                    ),                                                                  
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),                            
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
